5c8ae6cab381e55cd7aee6af5bc59c3deaa95e21
Aminub <Image de profil  Amine Naboulsi naboulsiamine10062001@gmail.com>
2023-01-26 17:52:52 +0100

    Grand commit  //ajout d'interface acceuil offline && ajout d'interface parametres &&ajout d'interface parametre de compte

--- app/src/main/java/com/example/applicationreservationcentre/MainActivity.java
+++ app/src/main/java/com/example/applicationreservationcentre/MainActivity.java
@@ -1,12 +1,20 @@
 package com.example.applicationreservationcentre;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.Menu;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.navigation.NavigationView;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
@@ -15,19 +23,24 @@ import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.applicationreservationcentre.databinding.ActivityMainBinding;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.squareup.picasso.Picasso;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity{
 
     private AppBarConfiguration mAppBarConfiguration;
     private ActivityMainBinding binding;
-
+    compoment_ compoment;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
-
+        compoment = new compoment_();
         setSupportActionBar(binding.appBarMain.toolbar);
         binding.appBarMain.fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -38,15 +51,36 @@ public class MainActivity extends AppCompatActivity {
         });
         DrawerLayout drawer = binding.drawerLayout;
         NavigationView navigationView = binding.navView;
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
+
         mAppBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow)
+                R.id.nav_home, R.id.nav_commentaire, R.id.nav_reservation)
                 .setOpenableLayout(drawer)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
         NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
         NavigationUI.setupWithNavController(navigationView, navController);
+        View headerView = navigationView.getHeaderView(0);
+        ImageView img = headerView.findViewById(R.id.slide_img_profil);
+        TextView nom = headerView.findViewById(R.id.slide_nom_profil);
+        TextView email = headerView.findViewById(R.id.slide_email_profil);
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+
+        DatabaseReference ref = FirebaseDatabase.getInstance().getReference("Comptes");
+        ref.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                Picasso.get().load(snapshot.child(compoment.get__id()).child("ref").getValue(String.class)).into(img);
+                nom.setText(snapshot.child(compoment.get__id()).child("nom").getValue(String.class));
+                email.setText(snapshot.child(compoment.get__id()).child("email").getValue(String.class));
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
     }
 
     @Override
@@ -61,5 +95,35 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
         return NavigationUI.navigateUp(navController, mAppBarConfiguration)
                 || super.onSupportNavigateUp();
+    }
+
+    @Override
+    public void onBackPressed() {
+        AlertDialog dialog = new AlertDialog.Builder(this).setTitle(R.string.quiter)
+                .setMessage(R.string.txt_quiter)
+                .setPositiveButton(R.string.ok , null)
+                .setNegativeButton(R.string.annuler , null)
+                .show();
+
+        Button ok_btn =dialog.getButton(AlertDialog.BUTTON_POSITIVE);
+        ok_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                MainActivity.this.finish();
+                System.exit(0);
+            }
+        });
+    }
+    public void click_setting(MenuItem item) {
+        if(item.getItemId() == R.id.setting_slide){
+            Intent it = new Intent(MainActivity.this , settingbar.class);
+            startActivity(it);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
     }
 }
---
+++ app/src/main/java/com/example/applicationreservationcentre/centre_item.java
@@ -0,0 +1,9 @@
+package com.example.applicationreservationcentre;
+
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public interface centre_item {
+
+    void centre_onclick_(int pos , ImageView imgcentre , TextView name  , TextView  map ,ImageView icon_map);
+}
